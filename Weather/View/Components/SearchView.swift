a69ea8ef80e0b04f0cac60cca14545db36f5c30d
Joe Vargas <joe.vargas.iosdev@gmail.com>
2024-08-27 11:24:55 -0500

    Created and applied SearchView

---
+++ Weather/View/Components/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Weather
+//
+//  Created by Joe Vargas on 8/27/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
