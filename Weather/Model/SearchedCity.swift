e5655e00fa8d68c4cd7c94c524315336547cc887
Joe Vargas <joe.vargas.iosdev@gmail.com>
2024-08-31 09:58:05 -0500

    Create SearchedCity model

---
+++ Weather/Model/SearchedCity.swift
@@ -0,0 +1,16 @@
+//
+//  SearchedCity.swift
+//  Weather
+//
+//  Created by Joe Vargas on 8/31/24.
+//
+
+import Foundation
+
+struct SearchedCity: Identifiable, Codable {
+    var id = UUID()
+    var name: String
+    var state: String
+    var latitude: Double
+    var longitude: Double
+}
