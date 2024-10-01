62fdc71f6f564a8f1def45f6adf3241211a1d2f7
Joe Vargas <joe.vargas.iosdev@gmail.com>
2024-10-01 08:36:24 -0500

    Applied a launch screen

--- Weather/App/WeatherApp.swift
+++ Weather/App/WeatherApp.swift
@@ -11,13 +11,27 @@ import SwiftData
 @main
 struct WeatherApp: App {
     @ObservedObject var locationManager = LocationManager.shared
+    @State private var isLoading = true
+    @State private var opacity = 0.0
     
     var body: some Scene {
         WindowGroup {
-            if locationManager.location == nil {
+            if isLoading {
+                LaunchScreenView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            withAnimation(.easeIn(duration: 1.0)) {
+                                isLoading = false
+                                opacity = 1.0
+                            }
+                        }
+                    }
+            } else if locationManager.location == nil {
                 LocationRequestView()
             } else if let location = locationManager.location {
                 TabViewContainer(location: location)
+                    .opacity(opacity)
+                
             }
         }
         .modelContainer(for: SearchedCity.self)
